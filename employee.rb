4a05e0704492a935c322b736db6d67ef0bc98af2
nathanknautz <nate.knautz@gmail.com>
2018-01-09 19:23:03 -0600

    initial commit

--- employee.rb
+++ employee.rb
@@ -1,19 +1,13 @@
-# employee_1 = {first_name: "Bruce", last_name: "Wayne", salary: 70000, active: true}
-# employee_2 = {first_name: "Diana", last_name: "Prince", salary: 80000, active: true}
-
-# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
-# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
-
 class Employee
   attr_reader :first_name, :last_name, :salary, :active
-  attr_writer :first_name, :last_name, :salary, :active
+  attr_writer :active
 
   def initialize(input_options)
     @first_name = input_options[:first_name]
     @last_name = input_options[:last_name]
     @salary = input_options[:salary]
-    @active = input_options[:active ]
-  end 
+    @active = input_options[:active]
+  end
 
   def print_info
     puts "#{first_name} #{last_name} makes #{salary} a year."
@@ -22,19 +16,66 @@ class Employee
   def give_annual_raise
     @salary = @salary * 1.05
   end
+end
 
-end 
-
-employee_1 = Employee.new(first_name: "Bruce", 
+employee_1 = Employee.new(
+                          first_name: "Bruce", 
                           last_name: "Wayne", 
-                          salary: 70000,
+                          salary: 70000, 
                           active: true
                           )
-employee_2 = Employee.new(first_name: "Diana", 
+
+employee_2 = Employee.new(
+                          first_name: "Diana", 
                           last_name: "Prince", 
-                          salary: 80000,
+                          salary: 80000, 
                           active: true
                           )
+# employee_2.print_info
+# p employee_1.active
+# employee_1.active = false
+# p employee_1.active
+
+class Manager < Employee
+  
+  def initialize(input_options)
+    super(input_options)
+    @employees = input_options[:employees]
+  end
+
+  def send_report
+    puts "Sending Email..."
+    #code that sends email
+    puts "Email sent."
+  end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise 
+    end 
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false 
+    end 
+  end 
+
+end
 
 
+manager = Manager.new(
+                      first_name: "Clark",
+                      last_name: "Kent",
+                      salary: 100000,
+                      active: true,
+                      employees: [employee_1, employee_2]
+                      )
+p employee_1
+p employee_2
 
+manager.give_all_raises
+manager.fire_all_employees
+
+p employee_1
+p employee_2
